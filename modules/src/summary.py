f83313569b04b0574e480791496a6bf489d02927
SID-PC\SiD <siddharth.nagrajann@gmail.com>
2017-03-17 23:00:15 +0530

    chat page alignment - summary details edit

--- modules/src/summary.py
+++ modules/src/summary.py
@@ -8,11 +8,12 @@ def get_summary(username,count=10):
     try:
         summary_list = []
         conn = sqlite3.connect(config.DATABASE)
-        cur = conn.execute('select amount,action from logs where username=? order by timestamp desc limit ?',[username,count])
+        cur = conn.execute('select amount,action,id from logs where username=? order by timestamp desc limit ?',[username,count])
         for row in cur.fetchall():
             summary_list.append({
             'amount' : row[0],
-            'action' : row[1]
+            'action' : row[1],
+            'id' : row[2]
             })
         return summary_list
     except Exception as e:
@@ -25,6 +26,7 @@ def get_list_html(summary_list):
     list_html = ""
     for item in summary_list:
         list_html += '<tr>'
+        list_html += '<td>' + str(item['id']) + '</td>'
         list_html += '<td>' + str(item['action']) + '</td>'
         list_html += '<td>' + str(item['amount']) + '</td>'
         list_html += '</tr>'
@@ -36,11 +38,15 @@ def process(user_input, nlu_data):
     count = 10
     if nlu_data['parameters']['number'] !='':
         count = int(nlu_data['parameters']['number'])
+        out_text ="The Last "+ str(count)  +" transactions on your account"
+    else:
+        out_text ="The Last few transactions on your account"
+
     summary_list = get_summary(user_input['username'],count)
-    out_text ="The Last "+ str(count)  +" transactions on your account"
     out_text += '''<table class="table table-condensed">
     <thead>
       <tr>
+        <th>TXN ID</th>
         <th>Action</th>
         <th>Amount</th>
       </tr>
