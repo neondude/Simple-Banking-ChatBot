fa9f0fd58ecd6d67f6a6905a73eba316622301c2
Siddharth Nagrajann <siddharth.nagrajann@gmail.com>
2017-03-17 21:26:54 +0530

    help text edited

--- modules/src/help.py
+++ modules/src/help.py
@@ -1,6 +1,6 @@
 def process(user_input, nlu_data):
-    #out_text = 'Hi there! I\'m the Banker, your banking assistant'
-    out_text = 'Here are some of the things i can do'
+    out_text = 'Hi there! I\'m the Banker, your banking assistant'
+    out_text += 'Here are some of the things i can do'
     out_text += '<br>  - help you withdraw money from your account'
     out_text += '<br>  - help you deposit money from your account'
     out_text += '<br>  - tell you your account balance'
